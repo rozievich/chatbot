440d21674fb1e6b2bfad8e66caf93c0997c69a2d
rozievich <oybekrozievich@gmail.com>
2025-02-06 21:43:53 +0500

    Update code by rozievich

--- chat/models.py
+++ chat/models.py
@@ -27,6 +27,7 @@ class ChatGroup(models.Model):
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=128)
     username = models.CharField(max_length=32, unique=True)
+    members = models.ManyToManyField(CustomUser, related_name="groups")
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -42,8 +43,8 @@ class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE)
     from_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     message = models.TextField()
-    is_delivery = models.DateTimeField(default=False)
-    is_read = models.BooleanField(default=False)
+    is_delivery = models.ManyToManyField(CustomUser, related_name="delivery_messages", blank=True)
+    is_read = models.ManyToManyField(CustomUser, related_name="delivery_messages", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
